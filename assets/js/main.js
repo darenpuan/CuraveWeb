5bea68eeb8cf1b5734f5ac980f68033096a123bd
Daren Puan <darenpuan@gmail.com>
2019-07-24 21:03:05 +0800

    Revert "update"
    This reverts commit fcde62821a2ae2995e46b61b85e092fea40767fd.

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,29 @@ $(document).ready(function () {
     });
 });
 
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var subject = document.forms["contactForm"]["subject"].value;
+    var msg = document.forms["contactForm"]["message"].value;
+    if (name == "") {
+        alert("Please fill in your name.");
+        return false;
+    }
+    if (email == "") {
+        alert("Please fill in your email address.");
+        return false;
+    }
+    if (subject == "") {
+        alert("Please fill in a subject.");
+        return false;
+    }
+    if (msg == "") {
+        alert("Please leave a short message for us.");
+        return false;
+    }
+}
+
 $(document).ready(function () {
     $('.modal').on('show.bs.modal', function () {
         if ($(document).height() > $(window).height()) {
